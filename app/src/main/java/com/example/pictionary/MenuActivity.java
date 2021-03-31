14a3ad06efb760726261ced506f1a901707aac97
indervir <inder_dhillon@sfu.ca>
2021-03-31 12:07:50 -0700

    Icon,logo, visuals added

--- app/src/main/java/com/example/pictionary/MenuActivity.java
+++ app/src/main/java/com/example/pictionary/MenuActivity.java
@@ -2,13 +2,29 @@ package com.example.pictionary;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MenuActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void friendsMenu_FriendsMenu(View view) {
+        Intent intent = new Intent(this, DrawingBoard.class);
+        startActivity(intent);
+        finish();
+
+    }
+
+    public void aboutUs_AboutUs(View view) {
+        Intent intent = new Intent(this, DrawingBoard.class);
+        startActivity(intent);
+        finish();
     }
 }
